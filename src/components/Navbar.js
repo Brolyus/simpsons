74c169e19f102d59aa380330df8a2c1b4cfbbe85
Brolyus <desousanthony@gmail.com>
2019-10-07 11:54:35 +0200

    adding Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar()  {
+    return (
+        <nav className = "Navbar">
+            <h2>THE SIMPSONS</h2>
+        </nav>
+    )
+}
+
+export default Navbar;
